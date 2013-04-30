ff88957adbe49ac9cbb4125c3822e7d816454887
Mike Mullen <m@m-mullen.com>
2013-04-30 10:38:10 -0700

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -28,6 +28,13 @@ describe "User pages" do
   		it "should not create a user" do
   			expect { click_button submit }.not_to change(User, :count)
   		end
+
+  		describe "after submission" do
+  			before { click_button submit }
+
+  			it { should have_selector('title', text: 'Sign up') }
+  			it { should have_content('error') }
+  		end
   	end
 
   	describe "with valid information" do
@@ -40,6 +47,11 @@ describe "User pages" do
 
   		it "should create a user" do
   			expect { click_button submit }.to change(User, :count).by(1)
+  		end
+
+  		describe "after saving the user" do
+  			before { click_button submit }
+  			it { should have_link('Sign out') }
   		end
   	end
   end
